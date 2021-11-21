bd6850f6ac8aaa76ab7d94049b6d776182e4f1a9
Sam Little <kagutaba256@gmail.com>
2021-11-21 13:14:32 -0800

    fixing setup file for root arch

--- dot_setup/arch/executable_arch-rootsetup.sh
+++ dot_setup/arch/executable_arch-rootsetup.sh
@@ -14,6 +14,12 @@ echo "Add password for user sam"
 passwd sam
 usermod -aG wheel sam
 pacman -S sudo --noconfirm
+visudo
+
+# get other script
+curl https://raw.githubusercontent.com/kagutaba256/dotfiles/main/dot_setup/arch/executable_arch-usersetup.sh -o user.sh
+chmod +x user.sh
+mv user.sh /home/sam/.
 
 # login as sam
 echo "log in as user sam"
