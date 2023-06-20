56c95ba657d125d5264857ffeb2da99058caf728
samlittl <samlittl@cisco.com>
2023-06-20 07:57:23 -0700

    adding newest config changes

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,55 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+M.formatters = {
+    -- javascript
+    javascript = {
+        -- npm install -g prettier
+        function()
+            return {
+                exe = "prettier",
+                args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--write'},
+                stdin = true
+            }
+        end,
+    },
+
+    -- json
+    json = {
+        -- npm install -g prettier
+        function()
+            return {
+                exe = "prettier",
+                args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--write'},
+                stdin = true
+            }
+        end,
+    },
+}
+
+M.options = {
+
+  autocmds = {
+    set_syntax_dosini = {
+      {"BufRead,BufNewFile", "*.config", "setfiletype dosini"},
+    },
+  },
+}
+
+return M
---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,12 @@
+require("custom.autocommands.syntax")
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+local opt = vim.opt
+opt.mouse = "r"
+
