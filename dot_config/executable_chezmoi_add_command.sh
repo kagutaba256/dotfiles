42395117a32038703443f20a12bcba90827af9a2
Sam Little <kagutaba256@gmail.com>
2023-12-18 14:08:04 -0800

    chezmoi adding config changes

---
+++ dot_config/executable_chezmoi_add_command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chezmoi add alacritty alias_def.sh env_def.sh bspwm btop doom dunst gitui htop hypr i3 kitty lf mpv ncmpcpp neofetch nvim nvm picom polybar rofi sxhkd tmux termite unipicker vim waybar xfce4 yarn yay zsh zathura
