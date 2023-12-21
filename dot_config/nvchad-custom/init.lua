c1e6e0fbb0618264ddc5f70dd27e1f3bc53bd388
Sam Little <kagutaba256@gmail.com>
2023-12-21 15:05:53 -0800

    adding latest chezmoi config stuff 2023/12/21

--- dot_config/nvchad-custom/init.lua
+++ dot_config/nvchad-custom/init.lua
@@ -9,4 +9,18 @@ require("custom.autocommands.syntax")
 
 local opt = vim.opt
 opt.mouse = "r"
-
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.backup = false
+opt.wrap = false
+opt.hlsearch = false
+opt.incsearch = true
+opt.termguicolors = true
+opt.expandtab = true
+opt.swapfile = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append('@-@')
+opt.updatetime = 50
--- dot_config/nvchad-custom/mappings.lua
+++ dot_config/nvchad-custom/mappings.lua
@@ -2,14 +2,27 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-    ["<C-z>"] = { "<cmd> undo<CR>", "undo" },
-  },
+    n = {
+        [";"] = { ":", "enter command mode", opts = { nowait = true } },
+        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+        ["<C-z>"] = { "<cmd> undo<CR>", "undo" },
+        ["<C-,>"] = { "<C-d>zz", "page down" },
+        ["<C-.>"] = { "<C-u>zz", "page up" },
+        ["J"] = { "mzJ`z", "combine below line and keep cursor" },
+        ["n"] = { "nzzzv", "search forwards" },
+        ["N"] = { "Nzzzv", "search backwards" },
+        ["<C-f>"] = { "<cmd>silent !t<CR>", "new tmux sessionizer" },
+    },
+    v = {
+        ["J"] = { ":m '>+1<CR>gv=gv", "move chunk down"},
+        ["K"] = { ":m '<-2<CR>gv=gv", "move chunk up"}
+    },
+    x = {
+        ["<leader>pp"] = {"\"_dP", "pastes over something"}
+    }
 }
 
 -- more keybinds!
